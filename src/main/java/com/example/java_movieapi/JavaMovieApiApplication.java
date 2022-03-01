e39b920a0d05df7e35a4e20343fa7bbc151a934d
Vilhard <ville.hotakainen@gmail.com>
2022-03-01 10:29:06 +0200

    initialize project

---
+++ src/main/java/com/example/java_movieapi/JavaMovieApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_movieapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMovieApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMovieApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java_movieapi/JavaMovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_movieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMovieApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
