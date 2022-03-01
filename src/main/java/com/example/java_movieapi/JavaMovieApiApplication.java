41ff974fe2f0702694e5ca18b413b3a45b9ab93e
miapeppi <peppi.makela@gmail.com>
2022-03-01 11:55:20 +0200

    Test commit

--- src/main/java/com/example/java_movieapi/JavaMovieApiApplication.java
+++ src/main/java/com/example/java_movieapi/JavaMovieApiApplication.java
@@ -8,6 +8,7 @@ public class JavaMovieApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaMovieApiApplication.class, args);
+        // test
     }
 
 }
