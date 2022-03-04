96af7279da58f06988db81f22d860d74e2a747b0
miapeppi <peppi.makela@gmail.com>
2022-03-04 14:07:24 +0200

    Added getMoviesInFranchise and getCharactersInFranchise

--- src/main/java/com/example/java_movieapi/Controller/FranchiseController.java
+++ src/main/java/com/example/java_movieapi/Controller/FranchiseController.java
@@ -1,14 +1,21 @@
 package com.example.java_movieapi.Controller;
 
 import com.example.java_movieapi.Model.CommonResponse;
+import com.example.java_movieapi.Model.Domain.Character;
 import com.example.java_movieapi.Model.Domain.Franchise;
+import com.example.java_movieapi.Model.Domain.Movie;
 import com.example.java_movieapi.Model.Dto.FranchiseDTO;
 import com.example.java_movieapi.Model.mapper.MapStructMapper;
 import com.example.java_movieapi.Repository.Interfaces.IFranchiseRepository;
+import com.example.java_movieapi.Repository.Interfaces.IMovieRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Tag(name = "Franchise")
@@ -16,10 +23,12 @@ import org.springframework.web.bind.annotation.*;
 public class FranchiseController {
     private final IFranchiseRepository franchiseRepo;
     private final MapStructMapper mapStructMapper;
+    private final IMovieRepository movieRepo;
 
-    public FranchiseController(IFranchiseRepository franchiseRepo, MapStructMapper mapStructMapper) {
+    public FranchiseController(IFranchiseRepository franchiseRepo, MapStructMapper mapStructMapper, IMovieRepository movieRepo) {
         this.franchiseRepo = franchiseRepo;
         this.mapStructMapper = mapStructMapper;
+        this.movieRepo = movieRepo;
     }
 
     @PostMapping("/franchise")
@@ -47,5 +56,31 @@ public class FranchiseController {
     public ResponseEntity<CommonResponse<String>> deleteFranchise(@PathVariable Integer id) {
          franchiseRepo.deleteById(id);
          return ResponseEntity.ok().body(new CommonResponse<>("Franchise deleted!"));
-     }
+    }
+
+    @GetMapping("/franchise/{id}/movies")
+    public ResponseEntity<CommonResponse<List<Movie>>> getAllMoviesInFranchise(@PathVariable Integer id) {
+        Franchise franchise = franchiseRepo.findById(id).get();
+        List<Movie> moviesInFranchise = franchise.getMovies();
+
+        return ResponseEntity
+                .ok()
+                .body(new CommonResponse<>(moviesInFranchise));
+    }
+
+    @GetMapping("/franchise/{id}/characters")
+    public ResponseEntity<CommonResponse<List<Character>>> getAllCharactersInFranchise(@PathVariable Integer id) {
+        Optional<Franchise> franchise = franchiseRepo.findById(id);
+        List<Character> charactersInFranchise = new ArrayList<>();
+        if(franchise.isPresent()) {
+            for(Movie movie: franchise.get().getMovies()) {
+                charactersInFranchise.addAll(movie.getCharacters());
+            }
+        }
+
+        return ResponseEntity
+                .ok()
+                .body(new CommonResponse<>(charactersInFranchise));
+    }
+
 }
