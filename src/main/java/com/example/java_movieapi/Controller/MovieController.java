2d2bd226a8d3a3ac558e81dd4edbe5eabe6f41f6
miapeppi <peppi.makela@gmail.com>
2022-03-04 14:39:44 +0200

    Added updateCharactersInMovie

--- src/main/java/com/example/java_movieapi/Controller/MovieController.java
+++ src/main/java/com/example/java_movieapi/Controller/MovieController.java
@@ -1,16 +1,23 @@
 package com.example.java_movieapi.Controller;
 
 import com.example.java_movieapi.Model.CommonResponse;
+import com.example.java_movieapi.Model.Domain.Character;
 import com.example.java_movieapi.Model.Domain.Movie;
 import com.example.java_movieapi.Model.Dto.MovieCreateDTO;
 import com.example.java_movieapi.Model.Dto.MovieDTO;
 import com.example.java_movieapi.Model.mapper.MapStructMapper;
 import com.example.java_movieapi.Repository.Impl.MovieRepositoryImpl;
+import com.example.java_movieapi.Repository.Interfaces.ICharacterRepository;
 import com.example.java_movieapi.Repository.Interfaces.IMovieRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @Tag(name ="Movie")
@@ -19,11 +26,13 @@ public class MovieController {
     private final MovieRepositoryImpl movieRepoCustom;
     private final IMovieRepository movieRepo;
     private final MapStructMapper mapStructMapper;
+    private final ICharacterRepository characterRepo;
 
-    public MovieController(MovieRepositoryImpl movieRepoCustom, IMovieRepository movieRepo, MapStructMapper mapStructMapper) {
+    public MovieController(MovieRepositoryImpl movieRepoCustom, IMovieRepository movieRepo, MapStructMapper mapStructMapper, ICharacterRepository characterRepo) {
         this.movieRepoCustom = movieRepoCustom;
         this.movieRepo = movieRepo;
         this.mapStructMapper = mapStructMapper;
+        this.characterRepo = characterRepo;
     }
 
     @PostMapping("/movie")
@@ -53,5 +62,22 @@ public class MovieController {
     public ResponseEntity<CommonResponse<String>> deleteMovie(@PathVariable Integer id) {
         movieRepo.deleteById(id);
         return ResponseEntity.ok().body(new CommonResponse<>("Movie deleted!"));
+    }
+
+    @PutMapping("/movie/{id}/characters")
+    public ResponseEntity<CommonResponse<Movie>> updateCharactersInMovie(@PathVariable Integer id, @RequestBody Integer[] characterId) {
+        Movie movie = movieRepo.findById(id).get();
+        List<Character> allCharacters = characterRepo.findAll();
+        List<Character> updatedCharacters = new ArrayList<>();
+        for(Character character: allCharacters) {
+            if(List.of(characterId).contains(character.getId())) {
+                updatedCharacters.add(character);
+            }
+        }
+        Set<Character> updatedCharacter = new HashSet<>(updatedCharacters);
+        movie.setCharacters(updatedCharacter);
+        return ResponseEntity
+                .ok()
+                .body(new CommonResponse<>(movie));
     }
 }
